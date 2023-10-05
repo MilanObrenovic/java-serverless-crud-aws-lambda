1f6435b1024757c1978eb112ad43262b3ba31777
Milan Obrenovic <mmobrenovic@gmail.com>
2023-10-05 22:36:16 +0200

    Base API endpoint - GET

--- src/main/java/com/serverless/Handler.java
+++ src/main/java/com/serverless/Handler.java
@@ -15,11 +15,16 @@ public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayRe
 	@Override
 	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
 		LOG.info("received: " + input);
-		Response responseBody = new Response("Go Serverless v1.x! Your function executed successfully!", input);
+
+		Response responseBody = new Response(
+				"Go Serverless v1.x! Your function executed successfully!",
+				input
+		);
+
 		return ApiGatewayResponse.builder()
 				.setStatusCode(200)
 				.setObjectBody(responseBody)
-				.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
+				.setHeaders(Collections.singletonMap("Content-Type", "application/json"))
 				.build();
 	}
 }
